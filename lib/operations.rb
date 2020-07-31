8078f1d5314d02296de0fcfdf14e948c6b6ddc5b
daniel campo <campo_daniel94@yahoo.com>
2020-07-31 21:26:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,13 +2,18 @@ require 'pry'
 
 def unsafe?(speed)
     if speed < 40 || speed > 60
-    puts unsafe?
+    return true
+    
+  else
+    speed > 40 || speed <60
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
